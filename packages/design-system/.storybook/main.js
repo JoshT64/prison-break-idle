6fd7df42184fda3002975cdbfe4afedabe3f4e6e
Joshua Toney <josh@Joshuas-MacBook-Air.local>
2023-09-30 12:59:35 -0400

    Config updates for static serving assets folder

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-themes',
   ],
+  staticDirs: ['../assets'],
+
   core: {
     builder: '@storybook/builder-vite',
   },
